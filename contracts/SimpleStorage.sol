1b71d9cef2a81e5f1f76b6a27c77800956caef59
Irakli <51046973+iraklishub@users.noreply.github.com>
2022-11-12 14:29:49 +0400

    sol basic mapping

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -6,20 +6,14 @@ contract simpleStorage {
         uint256 favoriteNumber;
         string name;
     }
-    
-    // People public people = People({favoriteNumber: 2, name: "John"});
+
+    mapping(string => uint256) public nameToFavoriteNumber;
 
     People[] public people;
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         people.push(People(_favoriteNumber, _name));
-
-        // people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
-
-        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
-        // People memory newPerson = People(_favoriteNumber, _name);
-        // people.push(newPerson);
+        nameToFavoriteNumber[_name] = _favoriteNumber;
     }
-
 
     // uint256 public favoriteNumber;
     // function store(uint256 _favoriteNumber) public {
