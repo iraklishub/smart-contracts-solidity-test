2ac791b15e3a77ccaafe446db49c4f86a0b06971
Irakli <51046973+iraklishub@users.noreply.github.com>
2022-11-12 14:09:43 +0400

    sol array and object

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,16 +1,33 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.7;
 
 contract simpleStorage {
-    uint256 public favoriteNumber;
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
+    
+    // People public people = People({favoriteNumber: 2, name: "John"});
 
-    function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
-        retrieve();
+    People[] public people;
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+
+        // people.push(People({favoriteNumber: _favoriteNumber, name: _name}));
+
+        // People memory newPerson = People({favoriteNumber: _favoriteNumber, name: _name});
+        // People memory newPerson = People(_favoriteNumber, _name);
+        // people.push(newPerson);
     }
 
-    function retrieve() public view returns(uint256) {
-        return favoriteNumber;
-    }
+
+    // uint256 public favoriteNumber;
+    // function store(uint256 _favoriteNumber) public {
+    //     favoriteNumber = _favoriteNumber;
+    // }
+
+    // function retrieve() public view returns(uint256) {
+    //     return favoriteNumber;
+    // }
 }
 
