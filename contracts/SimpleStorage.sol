66c9e0a92d291c4fc80863a00021235c8185cefa
Irakli <51046973+iraklishub@users.noreply.github.com>
2022-11-12 15:14:53 +0400

    first contract

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,26 +2,28 @@
 pragma solidity ^0.8.7;
 
 contract simpleStorage {
+    uint256 favoriteNumber;
+
+    mapping(string => uint256) public nameTofavouriteNumber;
+
     struct People {
         uint256 favoriteNumber;
         string name;
     }
 
-    mapping(string => uint256) public nameToFavoriteNumber;
-
     People[] public people;
-    function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        people.push(People(_favoriteNumber, _name));
-        nameToFavoriteNumber[_name] = _favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
     }
 
-    // uint256 public favoriteNumber;
-    // function store(uint256 _favoriteNumber) public {
-    //     favoriteNumber = _favoriteNumber;
-    // }
+    function retrieve() public view returns(uint256) {
+        return favoriteNumber;
+    }
 
-    // function retrieve() public view returns(uint256) {
-    //     return favoriteNumber;
-    // }
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameTofavouriteNumber[_name] = _favoriteNumber;
+    }
+
 }
-
