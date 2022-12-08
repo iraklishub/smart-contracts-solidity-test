ffe14e0c82ec9cd1d6a26e3f32853dbc36851382
Irakli <51046973+iraklishub@users.noreply.github.com>
2022-12-08 22:34:01 +0400

    importing/interactiong contracts, inheritance and overriding

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract simpleStorage {
+contract SimpleStorage {
     uint256 favoriteNumber;
 
     mapping(string => uint256) public nameTofavouriteNumber;
@@ -13,7 +13,7 @@ contract simpleStorage {
 
     People[] public people;
 
-    function store(uint256 _favoriteNumber) public {
+    function store(uint256 _favoriteNumber) public virtual {
         favoriteNumber = _favoriteNumber;
     }
 
